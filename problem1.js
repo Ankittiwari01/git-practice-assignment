54e50068bae9a36da0251c65d805906c33734087
Ankit kumar Tiwari <luckyankit310@gmail.com>
2022-09-14 22:16:02 +0530

    login logic

--- problem1.js
+++ problem1.js
@@ -1,2 +1,12 @@
 let reg_email="ankit@gmail.com";
 let reg_pas="123456";
+
+let ent_pas="123456";
+let ent_email="ankit@gmail.com";
+
+if(reg_email===ent_email && reg_pas===ent_pas){
+	connsole.log("Login successfull");
+}
+else{
+	console.log("Login Failed")
+}
